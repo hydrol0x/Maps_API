d2df485e6f22be8fe9dbbe92a48fcfae1dcae01c
J R <mrjacobry@gmail.com>
2023-02-10 09:09:34 -0500

    added functions for searching + env var

--- maps.py
+++ maps.py
@@ -1,23 +1,38 @@
-import googlemaps
-from datetime import datetime
+import os
+import requests
+import json
+from dotenv import load_dotenv
 
-gmaps = googlemaps.Client(key='AIzaSyDjB3EGGYVUSElFGzZEMnMel3fzZxDIVVk')
+load_dotenv()
 
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+API_KEY = os.environ.get('MAPS_API_KEY')
+SEARCH_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
+DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json?"
 
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
+def getID(userInput, inputType="textquery"):
+    response = requests.get(
+        SEARCH_URL+"input={userInput}&inputtype={inputType}&key={API_KEY}".format(userInput=userInput, inputType=inputType, API_KEY=API_KEY))
+    parsed = json.loads(response.text)
+    return parsed["candidates"][0]["place_id"]
 
-# Validate an address with address validation
-addressvalidation_result = gmaps.addressvalidation(['1600 Amphitheatre Pk'],
-                                                   regionCode='US',
-                                                   locality='Mountain View',
-                                                   enableUspsCass=True)
+
+def getPlace(placeID, fields="name,formatted_phone_number,formatted_address"):
+    response = requests.get(
+        DETAILS_URL+"place_id={placeID}&fields={fields}&key={API_KEY}".format(placeID=placeID, fields=fields, API_KEY=API_KEY))
+    parsed = json.loads(response.text)
+    return parsed["result"]
+
+
+def search(query, json=False):
+    PID = getID(query)
+    search = getPlace(PID)
+    if json:
+        return json.dumps(search)
+    else:
+        return search
+
+
+if __name__ == "__main__":
+    userPlace = input("Enter Query: ")
+    print([val for val in search(userPlace).values()])
